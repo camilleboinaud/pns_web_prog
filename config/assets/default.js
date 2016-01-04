025a17da6cde281a31ee1fa60581b5e372bc93f5
Tianhao Shi <shi.tianhao@etu.unice.fr>
2016-01-04 01:32:46 +0100

    add angular material

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,7 +5,8 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-material/angular-material.css'
       ],
       js: [
         'public/lib/angular/angular.js',
@@ -14,7 +15,9 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-file-upload/angular-file-upload.js'
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/angular-aria/angular-aria.js',
+        'public/lib/angular-material/angular-material.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
